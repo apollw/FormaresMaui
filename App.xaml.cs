81a9df70bd7baee3b1db509c52f6f0e9ae3abd49
apollw <apollw@gmail.com>
2024-10-09 04:30:42 -0300

    feat: MainPage Layout created

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,19 +1,14 @@
-﻿using FormaresMaui.Infrastructure.Licenses;
-using Microsoft.Extensions.Configuration;
-using System.Text.Json;
-
+﻿
 namespace FormaresMaui
 {
     public partial class App : Application
     {
         public App()
-        {
-            string SyncFusionKey = LicenseKey.GetSyncFusionKey();
-            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(SyncFusionKey);                       
+        {       
 
             InitializeComponent();
-
             MainPage = new AppShell();
+
         }
     }
 }
--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -7,5 +7,13 @@ namespace FormaresMaui
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            //Changes the NavigationBar and StatusBar  Colors in Android
+            Window.SetStatusBarColor(Android.Graphics.Color.LightGreen);
+            //Window.SetNavigationBarColor(Android.Graphics.Color.White);
+
+            base.OnCreate(savedInstanceState);
+        }
     }
 }
